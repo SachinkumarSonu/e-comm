60e7a00c554c14646782838e01a1d0271d0acb51
SachinkumarSonu <127083841+SachinkumarSonu@users.noreply.github.com>
2023-03-30 12:17:32 +0530

    Add files via upload

---
+++ registration/registration.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { EcomService } from '../ecom.service';
+
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+  registerForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,private service:EcomService ,private router:Router ) { }
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required,Validators.email]],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      password:['',Validators.required]
+      
+    });
+  }
+  register()
+  {
+    this.service.register(this.registerForm.value).subscribe({
+      next(value) {
+        console.log(value);
+        
+      },
+    })
+    this.router.navigate(['/login']);
+  }
+
+}
